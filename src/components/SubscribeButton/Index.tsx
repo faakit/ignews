bb14912f69197545611b77330b106cc4460962ea
faakit <64740422+faakit@users.noreply.github.com>
2021-11-08 15:07:18 -0300

    Updates

---
+++ src/components/SubscribeButton/Index.tsx
@@ -0,0 +1,43 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+import styles from './styles.module.scss';
+
+interface SubscribeButtonProps {
+    priceId: string;
+}
+
+type Session = {
+    sessionId: string;
+}
+
+export function SubscribeButton({priceId}:SubscribeButtonProps) {
+    const [session] = useSession()
+    async function handleSubscribe() {
+        if (!session){
+            signIn('github');
+            return;
+        }
+
+        try {
+            const response = await api.post('/subscribe');
+            const { sessionId } = response.data as Session;
+
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({sessionId});
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
+    return (
+        <button 
+            type="button"
+            className={styles.subscribeButton}
+            onClick={handleSubscribe}
+        >
+            Subscribe now
+        </button>
+    );
+}
